18313b29948196c69ff6bcdfa080f5ae44620548
Jorge Riquelme <jorge@larix.cl>
2020-06-13 01:18:15 -0400

    Solves parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
 
 
+def byte_parity(b: int) -> int:
+    c = 0
+    while b > 0:
+        c += 1 & b
+        b >>= 1
+    return c % 2
+
+
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    c = 0
+    cache = {}
+    for _ in range(8):
+        n = x & 255
+        if n not in cache:
+            cache[n] = byte_parity(n)
+        c += cache[n]
+        x >>= 8
+    return c % 2
 
 
 if __name__ == '__main__':
